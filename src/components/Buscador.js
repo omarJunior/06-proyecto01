02d6ade6c3b1730a7987886e65bf95b1283b4116
omarjunior <omarjunior041204@gmail.com>
2022-06-15 14:12:04 -0500

    ultimos commits actualizados

--- src/components/Buscador.js
+++ src/components/Buscador.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 export const Buscador = ({listadoState, setListadoState}) => {
 
   const [busqueda, setBusqueda] = useState('')
+  const [noEncontrado, setNoEncontrado] = useState(false)
 
 
   const buscarPeli = (e)=>{
@@ -13,10 +14,13 @@ export const Buscador = ({listadoState, setListadoState}) => {
     let pelis_encontradas = listadoState.filter(item => item.title.toLowerCase().includes(busqueda.toLowerCase()))
 
     //comprobar un resultado
-    if(busqueda.length <= 1){
+    if (busqueda.length <= 1 || pelis_encontradas.length <= 0){
       pelis_encontradas = JSON.parse(localStorage.getItem("pelis"))
-
+      setNoEncontrado(true)
+    }else{
+      setNoEncontrado(false)
     }
+
     //actualizar el estado del listado principal con lo que se filtra
     setListadoState(pelis_encontradas)
   }
@@ -24,6 +28,11 @@ export const Buscador = ({listadoState, setListadoState}) => {
   return (
     <div className="search">
         <h3 className="title">Buscador: {busqueda}</h3>
+        {
+          (noEncontrado) && busqueda.length > 1 && ( 
+            <span className="no-encontrado">No se ha encontrado ninguna coicindencia</span>
+          )
+        }
         <form>
             <input 
               type="text" 
